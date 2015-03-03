d66d52992d4ca533613035be552bfc51bfd6d36b
fxghqc <konxiao@gmail.com>
2015-03-03 17:04:46 +0800

    delete aside, footer and header. show clock.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,19 +1,41 @@
 var Ractive = require('ractive/ractive.runtime');
 
-var dashboard = new Ractive({
-  el: '#bench-container',
-  template: require('../templates/main.html'),
+var ractive = new Ractive({
+  el: clock,
+  template: require('../templates/clock.html'),
   data: {
-    name: '纪韵辰'
+    date: new Date(),
+    days: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ],
+    months: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
+
+    // add suffix to numbers, e.g. 2 -> '2nd', 14 -> '14th'
+    addSuffix: function ( num ) {
+      // special case - any number ending with 10-19 is suffixed with 'th'
+      if ( num % 100 >= 10 && num % 100 <= 19 ) {
+        return num + 'th';
+      }
+
+      switch ( num % 10 ) {
+        case 1: return num + 'st';
+        case 2: return num + 'nd';
+        case 3: return num + 'rd';
+      }
+
+      return num + 'th';
+    },
+
+    // ensure all numbers have two digits
+    pad: function ( num ) {
+      return ( num < 10 ? '0' + num : num );
+    },
+
+    // clock face markers - major (every 5 minutes) and minor (every minute)
+    major: new Array( 12 ),
+    minor: new Array( 60 )
   }
 });
 
-var benchTop = new Ractive({
-  el: '#bench-top',
-  template: require('../templates/header.html')
-});
-
-var benchLeft = new Ractive({
-  el: '#bench-left',
-  template: require('../templates/aside.html')
-});
+// ...then update it once a second
+setInterval( function () {
+  ractive.set( 'date', new Date() );
+}, 1000 );
