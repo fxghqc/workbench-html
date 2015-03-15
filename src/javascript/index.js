db35ac0a842aafd2bff945e909f1c0058464d85b
fxghqc <konxiao@gmail.com>
2015-03-15 14:57:49 +0800

    add lunar date.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,4 +1,5 @@
 var Ractive = require('ractive/ractive.runtime');
+var lunarCalendar = require('lunar-calendar');
 
 var ractive = new Ractive({
   el: clock,
@@ -31,11 +32,16 @@ var ractive = new Ractive({
 
     // clock face markers - major (every 5 minutes) and minor (every minute)
     major: new Array( 12 ),
-    minor: new Array( 60 )
+    minor: new Array( 60 ),
+    lunar: {}
   }
 });
 
 // ...then update it once a second
 setInterval( function () {
-  ractive.set( 'date', new Date() );
+  var date = new Date();
+  var lunar = lunarCalendar.solarToLunar(
+    date.getFullYear(), date.getMonth() + 1, date.getUTCDate());
+  ractive.set( 'date', date );
+  ractive.set( 'lunar',  lunar);
 }, 1000 );
